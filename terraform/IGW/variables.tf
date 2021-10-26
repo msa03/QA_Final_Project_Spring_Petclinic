f6439643fce6c5e6789f208eb6b23ea79a7e4833
J-62178 <James_Knowles.1998@Outlook.com>
2021-10-26 15:22:02 +0100

    Created IGW Terraform files.

---
+++ terraform/IGW/variables.tf
@@ -0,0 +1,15 @@
+variable "vpc_id"
+{
+    type        = string
+    description = "VPC ID"
+}
+
+variable "subnet_1"
+{
+    description = "Subnet 1 ID"
+}
+
+variable "subnet_2"
+{
+    description = "Subnet 2 ID"
+}
