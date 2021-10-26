5ea8f1d885d03edfa7856f605e6d094dd718448d
APBradford <88770635+APBradford@users.noreply.github.com>
2021-10-26 11:54:25 +0100

    Jenkins Pipeline

--- Jenkins/Scripts/jenkins_install.sh
+++ Jenkins/Scripts/jenkins_install.sh
@@ -3,6 +3,17 @@
 sudo apt-get update
 sudo apt-get upgrade -y
 
+sudo apt install curl -y
+curl https://get.docker.com | sudo bash
+sudo usermod -aG docker $(whoami)
+
+# download
+curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+# make executable
+chmod +x kubectl
+# install the binary onto your PATH
+sudo mv ./kubectl /usr/local/bin/kubectl 
+
 if type apt > /dev/null; then
     pkg_mgr=apt
     java="openjdk-8-jre"
